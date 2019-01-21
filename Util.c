040c9b272d4a638291ed80902234734e76c63eaf
Billy Su <g4691821@gmail.com>
2019-01-21 19:53:14 +0800

    Add parse_input()
    The parse_input() will transform the input string into Command_t
    form.

--- Util.c
+++ Util.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 #include "Util.h"
+#include "Command.h"
 
 void print_prompt() { printf("db > "); }
+
+/*
+ * This function received an output argument
+ * Return: category of the command
+ */
+int parse_input(char *input, Command_t *cmd) {
+    char *token;
+    token = strtok(input, " \n");
+    while (token != NULL) {
+        if (!strncmp(token, ".exit", 5)) {
+            cmd->type = BUILT_IN_CMD;
+        }
+        add_Arg(cmd, token);
+        token = strtok(NULL, " ");
+    }
+    return cmd->type;
+}
+
--- Util.h
+++ Util.h
@@ -1,6 +1,8 @@
 #ifndef DB_UTIL_H
 #define DB_UTIL_H
+#include "Command.h"
 
 void print_prompt();
+int parse_input(char *input, Command_t *cmd);
 
 #endif
