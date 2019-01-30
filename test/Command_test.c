37ff52d646f7246950632478a84912cf975d6f58
Billy Su <g4691821@gmail.com>
2019-01-30 22:02:57 +0800

    Extract test main() to all_test.c

--- test/Command_test.c
+++ test/Command_test.c
@@ -37,10 +37,3 @@ TEST(testCommand, testAddArg) {
     }
 }
 
-
-int main(int argc, char** argv) {
-    int iret;
-    testing::InitGoogleTest(&argc, argv);
-    iret = RUN_ALL_TESTS();
-    return iret;
-}
